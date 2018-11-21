6f367f0ec96cbc3f1642787b68c899ccdd9e4ebc
Saicoding <417353147@qq.com>
2018-11-21 11:30:54 +0800

    增加首选项设置

--- 金融类从业资格考试通/pages/mine/set/set.js
+++ 金融类从业资格考试通/pages/mine/set/set.js
@@ -5,43 +5,38 @@ Page({
    * 页面的初始数据
    */
   data: {
-    storageSize:0 
+    storageSize: 0,
+    checked: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     let self = this;
-      wx.getStorage({
-        key: 'user',
-        success: function (res) {
-        },
-        fail: function (res) {
-          console.log("获取数据失败");
-        }
-      });
+    wx.getStorage({
+      key: 'user',
+      success: function(res) {},
+      fail: function(res) {
+        console.log("获取数据失败");
+      }
+    });
 
-    let isOn = wx.getStorageSync('turnonWifiPrompt')==0?true:false;//检查非wifi开启状态
-    console.log(isOn)
-    if (isOn === "") {//如果没有本地缓存说明是第一次载入，默认是开启状态
-      isOn = true
-    }
-    console.log(isOn)
-    self.setData({
-      isOn:isOn
-    })
+    let myType = wx.getStorageSync('myType');
+
+    self.setSrcsAndColors(myType);
   },
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
     let self = this;
 
     this.loginOut = this.selectComponent('#loginOut');
     wx.getSystemInfo({ //得到窗口高度,这里必须要用到异步,而且要等到窗口bar显示后再去获取,所以要在onReady周期函数中使用获取窗口高度方法
-      success: function (res) { //转换窗口高度
+      success: function(res) { //转换窗口高度
         let windowHeight = res.windowHeight;
         let windowWidth = res.windowWidth;
         windowHeight = (windowHeight * (750 / windowWidth));
@@ -56,14 +51,14 @@ Page({
   /**
    * 清除缓存
    */
-  clearStorage:function(){
+  clearStorage: function() {
     let user = wx.getStorageSync('user');
     let storages = wx.getStorageInfoSync().keys;
 
-    for(let i = 0;i<storages.length;i++){
+    for (let i = 0; i < storages.length; i++) {
       console.log('hehe')
       let storage = storages[i];
-      if (storage == 'user' || storage == 'turnonWifiPrompt'){
+      if (storage == 'user' || storage == 'turnonWifiPrompt') {
         console.log('haha')
         continue;
       }
@@ -74,20 +69,20 @@ Page({
     let storages1 = wx.getStorageInfoSync();
     console.log(storages1)
     console.log(user)
-    
+
     wx.showToast({
       title: '清除缓存成功',
-      duration:2000
+      duration: 2000
     })
   },
 
   /**
    * 退出登录
    */
-  goOut:function(){
+  goOut: function() {
     wx.showModal({
-      content:'您确定要退出登录吗？',
-      confirmColor:'#0097f5',
+      content: '您确定要退出登录吗？',
+      confirmColor: '#0097f5',
       success(res) {
         if (res.confirm) {
           let url = encodeURIComponent('/pages/mine/mineIndex/mineIndex');
@@ -103,6 +98,62 @@ Page({
 
         }
       }
+    })
+  },
+
+  /**
+   * 改变首选项
+   */
+  categorySelect:function(e){
+    let self = this;
+    let value = e.detail.value;
+    let myType = self.data.myType;
+    
+    if(myType == value) return;
+
+    self.setSrcsAndColors(value);
+    wx.setStorageSync('myType', value);
+  },
+
+  /**
+   * 设置srcs和colors方法封装
+   */
+  setSrcsAndColors: function (myType) {
+    let self = this;
+    let srcs = {};
+    let colors = {};
+
+    switch (myType) {
+      case "zq":
+        srcs.zq = "/imgs/zq-s.png";
+        srcs.jj = "/imgs/jj.png";
+        srcs.qh = "/imgs/qh.png";
+        colors.zq = "#fd6131";
+        colors.jj = "black";
+        colors.qh = "black";
+        break;
+      case "jj":
+        srcs.zq = "/imgs/zq.png";
+        srcs.jj = "/imgs/jj-s.png";
+        srcs.qh = "/imgs/qh.png";
+        colors.zq = "black";
+        colors.jj = "#d2ac37";
+        colors.qh = "black";
+        break;
+      case "qh":
+        srcs.zq = "/imgs/zq.png";
+        srcs.jj = "/imgs/jj.png";
+        srcs.qh = "/imgs/qh-s.png";
+        colors.zq = "black";
+        colors.jj = "black";
+        colors.qh = "#c71585";
+        break;
+    }
+
+    self.setData({
+      srcs: srcs,
+      colors: colors,
+      myType: myType
     })
   },
 
